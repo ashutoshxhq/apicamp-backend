476ea020b46288259ed0abdfe1e012d7b9f22e7b
Ashutosh Dubey <idkashutosh@gmail.com>
2020-10-21 20:01:41 +0530

    data fetching for generating service

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,6 +1,14 @@
 package services
 
-import context "context"
+import (
+	"bytes"
+	context "context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
 // Server represents the gRPC server
 type Server struct {
@@ -9,5 +17,66 @@ type Server struct {
 // GenerateServiceCode fuctions generates service code
 func (s *Server) GenerateServiceCode(ctx context.Context, req *GenerateServiceCodeRequest) (*GenerateServiceCodeResponse, error) {
 
+	jsonData := map[string]string{
+		"query": `
+            { 
+                models {
+					name
+					service_id
+					updated_at
+					id
+					relationships {
+					  created_at
+					  deleted
+					  id
+					  model_field_id
+					  model_id
+					  name
+					  relationship_model_field_id
+					  relationship_model_id
+					  type
+					  updated_at
+					  relationshipModel {
+						id
+						name
+					  }
+					  relationshipModelField {
+						id
+						name
+						type
+					  }
+					  modelField {
+						id
+						name
+						type
+					  }
+					}
+					fields {
+					  id
+					  type
+					  name
+					  key
+					  created_at
+					  default
+					  deleted
+					  model_id
+					  null_value
+					  updated_at
+					}
+				  }
+            }
+        `,
+	}
+	jsonValue, _ := json.Marshal(jsonData)
+	request, err := http.NewRequest("POST", "https://apicamp-graphql.herokuapp.com/v1/graphql", bytes.NewBuffer(jsonValue))
+	request.Header.Add("x-hasura-admin-secret", `r5862n`)
+	client := &http.Client{Timeout: time.Second * 100}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	}
+	defer response.Body.Close()
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
 	return &GenerateServiceCodeResponse{Success: "generated service"}, nil
 }
--- templates/proto.template.go
+++ templates/proto.template.go
@@ -1,7 +1,7 @@
 package templates
 
-//Proto is template for proto file generation
-var Proto string = `syntax = "proto3";
+//ProtoHeader ...
+var ProtoHeader string = `syntax = "proto3";
 import "google/api/annotations.proto";
 package {{.Name}};
 option go_package = ".;{{.Name}}";
@@ -10,7 +10,11 @@ message Error {
 	string error = 1;
 	string message = 2;
 }
-{{range $i,$model := .Models}}
+
+`
+
+//ProtoMessages ...
+var ProtoMessages string = `
 
 message FilterField {
 	string type = 1;
@@ -18,152 +22,159 @@ message FilterField {
 }
 
 message Filter {
-	FilterField id = 1;{{range $index,$field := $model.Fields}}
+	FilterField id = 1;{{range $index,$field := .Fields}}
 	FilterField {{$field.Name}} = {{$index | addTwo }};{{end}}
 }
 
-message {{$model.Name | Title}} {
-	string id = 1;{{range $index,$field := $model.Fields}}
+message {{.Name | Title}} {
+	string id = 1;{{range $index,$field := .Fields}}
 	{{$field.Type}} {{$field.Name}} = {{$index | addTwo }};{{end}}
 }
 
-message Get{{$model.Name | Title}}Request{
+message Get{{.Name | Title}}Request{
 	Filter filter = 1;
-	{{if $model.IncludesPassword}}bool includePassword = 2;{{end}}
+	{{if .IncludesPassword}}bool includePassword = 2;{{end}}
 }
   
-message Get{{$model.Name | Title}}Response {
-	{{$model.Name | Title}} data = 1;
+message Get{{.Name | Title}}Response {
+	{{.Name | Title}} data = 1;
 	bool success = 2;
 	Error error = 3;
 }
 
-message GetMultiple{{$model.Name | Title}}Request {
+message GetMultiple{{.Name | Title}}Request {
 	repeated string ids = 1;
 	Filter filter = 2;
-	{{if $model.IncludesPassword}}bool includePassword = 3;{{end}}
+	{{if .IncludesPassword}}bool includePassword = 3;{{end}}
 }
   
-message GetMultiple{{$model.Name | Title}}Response {
-	repeated {{$model.Name | Title}} data = 1;
+message GetMultiple{{.Name | Title}}Response {
+	repeated {{.Name | Title}} data = 1;
 	bool success = 2;
 	Error error = 3;
 }
 
-message Create{{$model.Name | Title}}Request {
-	{{$model.Name | Title}} data = 1;
+message Create{{.Name | Title}}Request {
+	{{.Name | Title}} data = 1;
 }
 
-message Create{{$model.Name | Title}}Response {
+message Create{{.Name | Title}}Response {
 	string id = 1;
 	bool success = 2;
 	Error error = 3;
 }
 
-message CreateMultiple{{$model.Name | Title}}Request {
-	repeated {{$model.Name | Title}} data = 1;
+message CreateMultiple{{.Name | Title}}Request {
+	repeated {{.Name | Title}} data = 1;
 }
 
-message CreateMultiple{{$model.Name | Title}}Response {
+message CreateMultiple{{.Name | Title}}Response {
 	repeated string ids = 1;
 	bool success = 2;
 	Error error = 3;
 }
 
-message Update{{$model.Name | Title}}Request {
+message Update{{.Name | Title}}Request {
 	Filter filter = 1;
-	{{$model.Name | Title}} data = 2;
+	{{.Name | Title}} data = 2;
 }
 
-message Update{{$model.Name | Title}}Response {
+message Update{{.Name | Title}}Response {
 	bool success = 1;
 	Error error = 2;
 }
 
-message UpdateMultiple{{$model.Name | Title}}Request {
+message UpdateMultiple{{.Name | Title}}Request {
 	repeated string ids = 1;
 	Filter filter = 2;
-	{{$model.Name | Title}} data = 3;
+	{{.Name | Title}} data = 3;
 }
 
-message UpdateMultiple{{$model.Name | Title}}Response {
+message UpdateMultiple{{.Name | Title}}Response {
 	bool success = 1;
 	Error error = 2;
 }
 
-message Delete{{$model.Name | Title}}Request{
+message Delete{{.Name | Title}}Request{
 	string id = 1;
 	Filter filter = 2;
 }
 
-message Delete{{$model.Name | Title}}Response{
+message Delete{{.Name | Title}}Response{
 	bool success = 1;
 	Error error = 2;
 }
 
-message DeleteMultiple{{$model.Name | Title}}Request{
+message DeleteMultiple{{.Name | Title}}Request{
 	repeated string ids = 1;
 	Filter filter = 2;
 }
 
-message DeleteMultiple{{$model.Name | Title}}Response{
+message DeleteMultiple{{.Name | Title}}Response{
 	bool success = 1;
 	Error error = 2;
 }
-{{end}}
 
+`
+
+//ProtoServiceHeader ...
+var ProtoServiceHeader string = `
 service {{.Name}}Service {
-{{range $i,$model := .Models}}
-{{if $model.Crudgen }}
-	rpc Get{{$model.Name | Title}}(Get{{$model.Name | Title}}Request) returns (Get{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/get"
-			body: "*"
-		};
-	}
-	rpc GetMultiple{{$model.Name | Title}}(GetMultiple{{$model.Name | Title}}Request) returns (GetMultiple{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/getMultiple"
-			body: "*"
-		};
-	}
-	rpc Create{{$model.Name | Title}}(Create{{$model.Name | Title}}Request) returns (Create{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/create"
-			body: "*"
-		};
-	}
-	rpc CreateMultiple{{$model.Name | Title}}(CreateMultiple{{$model.Name | Title}}Request) returns (CreateMultiple{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/createMultiple"
-			body: "*"
-		};
-	}
-	rpc Update{{$model.Name | Title}}(Update{{$model.Name | Title}}Request) returns (Update{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/update"
-			body: "*"
-		};
-	}
-	rpc UpdateMultiple{{$model.Name | Title}}(UpdateMultiple{{$model.Name | Title}}Request) returns (UpdateMultiple{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/updateMultiple"
-			body: "*"
-		};
-	}
-	rpc Delete{{$model.Name | Title}}(Delete{{$model.Name | Title}}Request) returns (Delete{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/delete"
-			body: "*"
-		};
-	}
-	rpc DeleteMultiple{{$model.Name | Title}}(DeleteMultiple{{$model.Name | Title}}Request) returns (DeleteMultiple{{$model.Name | Title}}Response) {
-		option (google.api.http) = {
-			post: "/v1/{{$model.Name}}/deleteMultiple"
-			body: "*"
-		};
-	}
-	{{end}}
-{{end}}
+`
+
+//ProtoServiceMethods ...
+var ProtoServiceMethods string = `
+rpc Get{{.Name | Title}}(Get{{.Name | Title}}Request) returns (Get{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/get"
+		body: "*"
+	};
+}
+rpc GetMultiple{{.Name | Title}}(GetMultiple{{.Name | Title}}Request) returns (GetMultiple{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/getMultiple"
+		body: "*"
+	};
+}
+rpc Create{{.Name | Title}}(Create{{.Name | Title}}Request) returns (Create{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/create"
+		body: "*"
+	};
+}
+rpc CreateMultiple{{.Name | Title}}(CreateMultiple{{.Name | Title}}Request) returns (CreateMultiple{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/createMultiple"
+		body: "*"
+	};
+}
+rpc Update{{.Name | Title}}(Update{{.Name | Title}}Request) returns (Update{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/update"
+		body: "*"
+	};
+}
+rpc UpdateMultiple{{.Name | Title}}(UpdateMultiple{{.Name | Title}}Request) returns (UpdateMultiple{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/updateMultiple"
+		body: "*"
+	};
+}
+rpc Delete{{.Name | Title}}(Delete{{.Name | Title}}Request) returns (Delete{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/delete"
+		body: "*"
+	};
+}
+rpc DeleteMultiple{{.Name | Title}}(DeleteMultiple{{.Name | Title}}Request) returns (DeleteMultiple{{.Name | Title}}Response) {
+	option (google.api.http) = {
+		post: "/v1/{{.Name}}/deleteMultiple"
+		body: "*"
+	};
+}
+`
+
+//ProtoServiceFooter ...
+var ProtoServiceFooter string = `
 }
 `
